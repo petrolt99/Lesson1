3a5095df753c26f40746805d7a78b3b3ea93d4f8
petrolt99 <petrolt99@gmail.com>
2020-11-15 20:59:47 +0500

    Homework

---
+++ task1.js
@@ -0,0 +1,12 @@
+"use strict";
+
+alert("Задание №1");
+let input = prompt("Введите строку:", "");
+
+let capitalise = (str) => {
+  let temp = str.charAt(0).toUpperCase() + str.slice(1);
+    return temp;
+}
+
+let output = capitalise(input);
+console.log(output);
